c5fcdc493a4a6232c8f0b6a8abdc86354791e710
Sheldon Davis <sheldon.davis1515@gmail.com>
2026-02-27 09:41:31 -0500

    update pages to use new form component

--- app/routes/ContactsRentals/contactUs.tsx
+++ app/routes/ContactsRentals/contactUs.tsx
@@ -1,10 +1,8 @@
 //* IMPORTS
 import type { Route } from './+types/contactUs';
-import { type Key } from 'react';
-// import { db } from '~/config/firebaseConfig';
-// import { collection, getDocs, query, where, limit, documentId } from 'firebase/firestore';
-import type { VFC_Block } from '~/customTypes/Page';
 import Article from '~/Components/Article';
+import PageBreakWrapper from '~/Components/PageBreakWrapper';
+import RequestForm from '~/Components/RequestForm';
 
 //import page data
 import { ContactUs_Data } from '~/database/ContactsRentals/ContactUs_Data';
@@ -13,12 +11,7 @@ export function meta({}: Route.MetaArgs) {
   return [{ title: 'Alum Bank Community Volunteer Fire Company' }, { name: 'description', content: 'Welcome to React Router!' }];
 }
 
-export async function clientLoader({ params }: Route.LoaderArgs) {
-  // const q = query(collection(db, 'Pages'), where(documentId(), '==', `Contact Us`), limit(1));
-  // const querySnapshot = await getDocs(q);
-  // const tempData = querySnapshot.docs[0].data().elements;
-  // return await { elements: tempData };
-}
+export async function clientLoader({ params }: Route.LoaderArgs) {}
 
 export default function ContactUs({ loaderData }: Route.ComponentProps) {
   return (
@@ -27,6 +20,11 @@ export default function ContactUs({ loaderData }: Route.ComponentProps) {
       {ContactUs_Data.elements.map((block, index) => {
         return <Article key={index} data={block} />;
       })}
+      <PageBreakWrapper>
+        <p>OR</p>
+      </PageBreakWrapper>
+
+      <RequestForm submitBtnText='Submit Contact form' txtAreaLBL='Your Message' formName='Contact' formFriendlyName='Contact Us' />
       {/* {loaderData.elements.map((block: VFC_Block, i: Key | null | undefined) => (
         <Article key={i} data={block} />
       ))} */}
