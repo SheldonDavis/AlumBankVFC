a9672c03d0edd20cca439bae605501344b19fd70
Sheldon Davis <sheldon.davis1515@gmail.com>
2026-05-08 01:07:38 -0400

    adjust spacing and mobile menu stylings

--- app/Components/PageBreakWrapper.tsx
+++ app/Components/PageBreakWrapper.tsx
@@ -2,7 +2,7 @@ import { type ReactNode } from 'react';
 
 export default function PageBreakWrapper({ children }: { children: ReactNode }) {
   return (
-    <div className={`flex flex-row items-center w-full gap-4`}>
+    <div className={`flex flex-row items-center w-full gap-4 mb-8`}>
       <div className={`w-full bg-(--color-header) p-[0.13em]`}></div>
       <div>{children}</div>
       <div className={`w-full bg-(--color-header) p-[0.13em]`}></div>
